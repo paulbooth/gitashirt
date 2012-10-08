9855a0f9b508551f7bae998d8783934fff893192
Tim Cameron Ryan <id@timryan.org>
2012-10-08 14:41:42 -0400

    Support .gitshirt in a crude way

--- app.js
+++ app.js
@@ -15,11 +15,26 @@ rem.console(gh, {
 
   // List user gists.
   user('user').get(function (err, profile) {
-    user("users", profile.login, "repos").get(function (err, json) {
+    user("users", profile.login, "repos").get({per_page: 100}, function (err, json) {
+      var data = {};
+      json.forEach(function (repo) {
+        data[repo.full_name] = 'tx1=' + repo.full_name;
+        rem.url('https://raw.github.com/' + repo.full_name + '/master/.gitshirt').get(function (req, res) {
+          if (res.statusCode == 200) {
+            rem.consume(res, function (bin) {
+              if (String(bin).indexOf('<html') == -1) {
+                console.log('Loaded .gitshirt from ' + repo.full_name);
+                data[repo.full_name] = bin;
+              }
+            })
+          }
+        });
+      });
+
       app.get('/repos', function (req, res) {
         res.setHeader('content-type', 'text/html')
-        res.send(json.map(function (repo) {
-          return '<a href="/#tx1=' + repo.full_name + '">' + repo.full_name + '</a><br>';
+        res.send(Object.keys(data).map(function (key) {
+          return '<b>' + key + '</b>: <a href="/#' + data[key] + '">' + key + '</a><br>';
         }).join(''));
       });
     });
